f3f480be3e0e1ba39045a32650437cf509da00c3
Stratimirovic <97814549+Stratimirovic@users.noreply.github.com>
2023-10-25 14:23:21 -0400

    Add files via upload

---
+++ as9_3blink-1.ino
@@ -0,0 +1,25 @@
+void setup() {
+pinMode(0, OUTPUT);    // sets the digital pin 0 as output
+pinMode(1, OUTPUT);    // sets the digital pin 1 as output
+pinMode(2, OUTPUT);    // sets the digital pin 2 as output
+pinMode(9, INPUT);    // sets the digital pin 9 as input
+}
+
+void loop() {
+  
+if (digitalRead(9) == HIGH) {
+  digitalWrite(0, HIGH); // sets the digital pin 0 on
+  delay(20000);            // waits for a second
+  digitalWrite(0, LOW);  // sets the digital pin 0 off
+  delay(20000);            // waits for a second
+    digitalWrite(1, HIGH); // sets the digital pin 1 on
+  delay(20000);            // waits for a second
+  digitalWrite(1, LOW);  // sets the digital pin 1 off
+  delay(20000);            // waits for a second
+    digitalWrite(2, HIGH); // sets the digital pin 2 on
+  delay(20000);            // waits for a second
+  digitalWrite(2, LOW);  // sets the digital pin 2 off
+  delay(20000);            // waits for a second
+}
+else{}
+}
